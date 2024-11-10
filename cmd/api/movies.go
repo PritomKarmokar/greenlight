0e27f8b5fc4510cc117e4b28da33c6a51e66d7c4
Pritom Karmokar <pritomkarmokar99@gmail.com>
2024-11-10 23:22:10 +0600

    UnmarshalJSON() method added for additional unmarshaling options

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,10 +10,10 @@ import (
 // using `Decode`
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	//err := json.NewDecoder(r.Body).Decode(&input)
