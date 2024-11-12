621f3a1497f75bf47c014bab9af3b4881834a99d
Pritom Karmokar <pritomkarmokar99@gmail.com>
2024-11-12 21:22:19 +0600

    adding two missing validation rules for runtime

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -23,8 +23,11 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
 	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
 
-	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must not be provided")
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
-	v.Check(len(movie.Genres) <= 5, "genres", "must not contain at least 5 genres")
-	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate genres")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
