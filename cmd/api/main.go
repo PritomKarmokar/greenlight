7532fec3a7972218fc83bd379cea74c35f71c7ea
Pritom Karmokar <pritomkarmokar99@gmail.com>
2024-11-16 23:32:33 +0600

    feat: movies CRUD added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"greenlight.net/internal/data"
 
 	_ "github.com/lib/pq"
 	"log"
@@ -32,6 +33,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -78,6 +80,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
