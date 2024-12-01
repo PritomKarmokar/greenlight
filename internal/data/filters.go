6cf148166334cf03b547a4802b872d0870df6f54
Pritom Karmokar <pritomkarmokar99@gmail.com>
2024-12-02 00:20:39 +0600

    feat: adding sorting list

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,9 @@
 package data
 
-import "greenlight.net/internal/validator"
+import (
+	"greenlight.net/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
@@ -16,4 +19,20 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort list")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
