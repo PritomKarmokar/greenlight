0d4f367a32ecb33a0040e6d1bfff64e55048dd96
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-06-16 00:03:38 +0600

    feat: project initiated

---
+++ cmd/api/movies.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"greenlight/internal/data"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create a new movie\n")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"genre", "romance", "war"},
+		Version:   "1.0.0",
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
